f6b9b4c5e32cbaa1a6fadea0ce7de3f77201f01a
Mohdzaiduddinzyan <mohd.zaid.zyan@gmail.com>
2026-09-19 22:09:05 +0530

    Correct JSON object creation and file writing
    Fix issues with object creation and JSON dumping.

--- create_json.py
+++ create_json.py
@@ -1,1 +1,15 @@
+import json
 
+n= int(input("Enter number of objects: "))
+f= int(input("enter number of fields: "))
+l=[]
+
+for i in range(n):
+  d={}
+  for j in range(f):
+    k = input("Enter key: ")
+    v= input("Enter value: ")
+    d[k]=v
+  d.append(d)
+with open("sample.json","w") as f:
+  json.dump(d,f)
